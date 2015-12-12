b1f725e6d79cf922748fd5d6f38285821294a2d9
zfeidy <zfeidy>
2015-12-12 18:30:31 +0800

    组件化

---
+++ tools/host.js
@@ -0,0 +1,9 @@
+/* global module */
+
+var hostconfig ={
+    host: "http://localhost:3000",
+    auth_host: "http://localhost:3000"
+};
+
+module.exports = hostconfig;
+document.domain = "localhost";
