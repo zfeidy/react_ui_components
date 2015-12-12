3161adf3fa4d003df5fabf0a04f79dc31671534c
zfeidy <zfeidy>
2015-12-12 17:17:17 +0800

    组件es6化

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import WcTable from './lib/JingTable';
+import WcTooltip from "./lib/JingTooltip";
+import AutoComplete from "./lib/JingAutoComplete";
+import JingInfoTable from "./lib/JingInfoTable";
+import SmartInput from "./lib/JingSmartInput";
+import DynKVInput from "./lib/JingDynKVInput";
+import { SplitLine, RadioGroup, Selector, ButtonList, SplitLine } from '../components/JingInput';
+
+export WcTable;
+export WcTooltip;
+export AutoComplete;
+export JingInfoTable;
+export SmartInput;
+export DynKVInput;
+export SplitLine;
+export RadioGroup;
+export Selector;
+export ButtonList;
+export SplitLine;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* global module, __dirname */
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        login: ['webpack-hot-middleware/client', './src/containers/Login'],
+        index: ['webpack-hot-middleware/client', './src/containers/Index']
+    },
+    output: {
+        path: path.join(__dirname, 'src', 'dist'),
+        filename: '[name].js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname},
+            {test: /\.css?$/, loaders: ['style', 'raw'], include: __dirname},
+            {test: /\.jsx$/, loader: 'babel-loader!jsx-loader?harmony'},
+            {test: /\.(woff|woff2)$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
+            {test: /\.ttf$/, loader: "file-loader"},
+            {test: /\.eot$/, loader: "file-loader"},
+            {test: /\.svg$/, loader: "file-loader"}
+        ]
+    }
+};
