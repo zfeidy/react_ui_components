f7646f7ce15887e95e0359f230d1ddadb3bb5ce6
falali <falali@wacai.com>
2016-01-12 13:18:51 +0800

    优化组件

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import AutoComplete from './lib/JingAutoComplete';
 import InfoTable from './lib/JingInfoTable';
 import SmartInput from './lib/JingSmartInput';
 import DynKVInput from './lib/JingDynKVInput';
+import MultipleSelector from './lib/JingMultipleSelector';
 import { CheckBoxGroup, RadioGroup, Selector, ButtonList, SplitLine } from './lib/JingInput';
 
-export { WcTable, WcTooltip, AutoComplete, InfoTable, SmartInput, DynKVInput, CheckBoxGroup, RadioGroup, Selector, ButtonList, SplitLine };
+export { WcTable, WcTooltip, AutoComplete, InfoTable, SmartInput, DynKVInput, CheckBoxGroup, RadioGroup, Selector, ButtonList, SplitLine, MultipleSelector };
